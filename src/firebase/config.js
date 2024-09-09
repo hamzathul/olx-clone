b376289e54d0ef4a0d55874cc3a7f9a307af8b3f
Hamzathul Favas E <ehamzathulfavas@gmail.com>
2024-09-09 16:26:13 +0530

    right now the firebase context is not working

---
+++ src/firebase/config.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCX-FPNbbXCuFppKcYih5TxvADi6ipR8kM",
+    authDomain: "fir-d80ce.firebaseapp.com",
+    projectId: "fir-d80ce",
+    storageBucket: "fir-d80ce.appspot.com",
+    messagingSenderId: "1072731959063",
+    appId: "1:1072731959063:web:f83931f4695d30636bd152",
+    measurementId: "G-NY2WK0J0B2"
+  };
+
+export default firebase.initializeApp(firebaseConfig)
